1e0383049160367002aa5aa62e982a20d317ffeb
DAVID WINGQVIST <daag19@student.bth.se>
2021-03-31 11:11:42 +0200

    Game class bas.

---
+++ Prodigium/Game.cpp
@@ -0,0 +1,19 @@
+#include "Game.h"
+
+Game::Game()
+{
+	isRunning = true;
+}
+
+Game::~Game()
+{
+}
+
+void Game::run()
+{
+	// Active game loop.
+	if (isRunning)
+	{
+
+	}
+}
---
+++ Prodigium/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <direct.h>
+#include <DirectXMath.h>
+
+class Game
+{
+private:
+
+	bool isRunning;
+
+public:
+
+	Game();
+	~Game();
+
+	void run();
+
+};
