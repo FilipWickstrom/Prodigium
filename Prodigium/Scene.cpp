922b06f11582db3ba029ac248df6420de33bc571
DAVID WINGQVIST <daag19@student.bth.se>
2021-04-16 14:37:15 +0200

    Scene(): Add och Render

--- Prodigium/Scene.cpp
+++ Prodigium/Scene.cpp
@@ -6,16 +6,30 @@ Scene::Scene()
 
 Scene::~Scene()
 {
+	for (int i = 0; i < (int)objects.size(); i++)
+	{
+		objects.pop_back();
+	}
 }
 
-void Scene::Add(std::string filePath, float x, float y, float z, float rotation)
+void Scene::Add(ID3D11Device*& device, std::string filePath, DirectX::XMFLOAT3 pos,
+	DirectX::XMFLOAT3 rotation, DirectX::XMFLOAT3 scale)
 {
+	MeshObject newObject;
+	newObject.BuildMatrix(device, pos, scale, rotation);
+
+	objects.push_back(&newObject);
 }
 
 void Scene::Remove(int index)
 {
+
 }
 
 void Scene::Render(ID3D11DeviceContext*& context)
 {
+	for (int i = 0; i < (int)objects.size(); i++)
+	{
+		objects[i]->Render(context);
+	}
 }
--- Prodigium/Scene.h
+++ Prodigium/Scene.h
@@ -2,19 +2,23 @@
 #include <d3d11.h>
 #include <vector>
 #include <string>
+#include <DirectXMath.h>
+#include "MeshObject.h"
 
 class Scene
 {
 private:
 
 	// Vector for all the objects present in this scene.
+	std::vector<MeshObject*> objects;
 
 public:
 
 	Scene();
 	virtual ~Scene();
 
-	void Add(std::string filePath, float x, float y, float z, float rotation);
+	void Add(ID3D11Device*& device, std::string filePath, DirectX::XMFLOAT3 pos = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), 
+		DirectX::XMFLOAT3 rotation = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), DirectX::XMFLOAT3 scale = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f));
 	void Remove(int index);
 	void Render(ID3D11DeviceContext*& context);
 
