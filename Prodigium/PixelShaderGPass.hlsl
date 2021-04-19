0dcdfafa12375015d51fe59ca791b1cb948a84d3
FilipWickstrom <fiai18@student.bth.se>
2021-04-19 11:27:23 +0200

    Testing assimp model

--- Prodigium/PixelShaderGPass.hlsl
+++ Prodigium/PixelShaderGPass.hlsl
@@ -1,24 +1,31 @@
+Texture2D diffuseTexture : register(t0);
+//Normalmap
+
+SamplerState anisotropic : register(s0);
+
 struct PixelShaderInput
 {
-    float4 position : SV_Position;
-    float2 texCoord : TEXCOORD;
-    float3 normal : NORMAL;
+    float4 position     : SV_Position;
+    float2 texCoord     : TEXCOORD;
+    float3 normal       : NORMAL;
 };
 
 struct PixelShaderOutput
 {
-    float3 position : SV_Target0;
-    float2 texCoord : SV_Target1;
-    float3 normal : SV_Target2;
+    float4 position : SV_Target0;
+    float4 uv  : SV_Target1;
+    float4 normal   : SV_Target2;
 };
 
 PixelShaderOutput main(PixelShaderInput input)
 {
     PixelShaderOutput output;
     
-    output.position = input.position.xyz;
-    output.texCoord = input.texCoord;
-    output.normal = input.normal;
+    output.position = input.position;
+    
+    output.uv = diffuseTexture.Sample(anisotropic, input.texCoord);
+    
+    output.normal = float4(input.normal, 1.0f);
     
     return output;
 }
--- Prodigium/PixelShaderLightPass.hlsl
+++ Prodigium/PixelShaderLightPass.hlsl
@@ -1,10 +1,24 @@
+
+Texture2D GBuff_Position    : register(t0);
+Texture2D GBuff_Texture     : register(t1);
+Texture2D GBuff_Normal      : register(t2);
+//Material?
+
+SamplerState anisotropic    : register(s0);
+
 struct PixelShaderInput
 {
     float4 position : SV_Position;
     float2 texCoord : TEXCOORD;
 };
 
-float4 main() : SV_TARGET
+float4 main(PixelShaderInput input) : SV_TARGET
 {
-    return float4(1.0f, 0.0f, 0.0f, 1.0f);
+    float4 finalColour = {0,0,0,0};
+    
+    //Sample the texture - TESTING
+    //finalColour = GBuff_Texture.Sample(anisotropic, input.texCoord);
+    
+    return finalColour;
+    //return float4(1.0f, 0.0f, 0.0f, 1.0f);
 }
