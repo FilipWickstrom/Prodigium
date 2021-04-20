aa1ac2d199162a2eed4a3f5ab8b38e28d83c6b48
DAVID WINGQVIST <daag19@student.bth.se>
2021-04-20 11:05:37 +0200

    Further Work on Scene/SceneHandler

--- Prodigium/SceneHandler.h
+++ Prodigium/SceneHandler.h
@@ -11,11 +11,18 @@ public:
 	SceneHandler();
 	virtual ~SceneHandler();
 
+	// Adds a new fresh scene into the vector<> call this when you want to init a new scene.
 	void AddScene();
 
+	// removes the scene at the indexed begin() + index spot.
+	void RemoveScene(int index);
+
+	// Get access to the indexed scene to edit it.
 	Scene& EditScene(int index);
 
+	// return the number of Scenes currently existing in the game.
 	int GetNrOfScenes() const;
 
+	// Render the scene with the sceneIndex.
 	void Render(ID3D11DeviceContext*& context, int sceneIndex);
 };
