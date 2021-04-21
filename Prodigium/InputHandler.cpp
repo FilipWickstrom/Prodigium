c3982cc0a9a6c91aa35cec2b99e14f9b9a9d53ef
Swizzman <swizzman123@gmail.com>
2021-04-21 11:24:30 +0200

    WindowProc now moved to InputHandler

--- Prodigium/InputHandler.cpp
+++ Prodigium/InputHandler.cpp
@@ -73,3 +73,27 @@ LRESULT InputHandler::WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
 	// Handle any messages the switch statement didn't by using default methodology
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
+
+LRESULT InputHandler::WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	// Sort through and find what code to run for the message given
+	switch (message)
+	{
+		// This message is read when the window is closed
+	case WM_DESTROY:
+		// Tell calling thread to terminate
+		PostQuitMessage(0);
+		return 0;
+	case WM_KEYDOWN:
+		break;
+	case WM_MOUSEMOVE:
+		break;
+	case WM_LBUTTONDOWN:
+		break;
+	case WM_RBUTTONDOWN:
+		break;
+	}
+
+	// Handle any messages the switch statement didn't by using default methodology
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
