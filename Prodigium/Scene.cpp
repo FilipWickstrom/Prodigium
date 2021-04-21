14b28fc5d92861e1b898eb612247e7737607a60d
DAVID WINGQVIST <daag19@student.bth.se>
2021-04-21 11:18:06 +0200

    Scene/SceneHandler

--- Prodigium/Scene.cpp
+++ Prodigium/Scene.cpp
@@ -2,20 +2,58 @@
 
 Scene::Scene()
 {
+	this->currentObject = 0;
 }
 
 Scene::~Scene()
 {
 }
 
-void Scene::Add(std::string filePath, float x, float y, float z, float rotation)
+void Scene::Add(ID3D11Device*& device, std::string filePath, DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 rotation, DirectX::XMFLOAT3 scale)
 {
+	/*
+		Create a new MeshObject from input.
+	*/
+
+	MeshObject* newObject = new MeshObject;
+	newObject->BuildMatrix(device, pos, scale, rotation);
+	//newObject->Initialize(device, filePath + ".obj");
+
+	this->objects.push_back(newObject);
+	this->currentObject = (int)objects.size() - 1;
+}
+
+void Scene::UpdateMatrix(ID3D11Device*& device, DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 rotation, DirectX::XMFLOAT3 scale)
+{
+	/*
+		Update the Matrix with input.
+	*/
+
+
 }
 
 void Scene::Remove(int index)
 {
+	this->currentObject = 0;
+	this->objects.erase(objects.begin() + index);
+}
+
+void Scene::SwitchObject(int index)
+{
+	if (index < (int)this->objects.size() && index >= 0)
+		this->currentObject = index;
+}
+
+int Scene::GetNumberOfObjects() const
+{
+	return (int)this->objects.size();
 }
 
 void Scene::Render(ID3D11DeviceContext*& context)
 {
+	for (int i = 0; i < (int)this->objects.size(); i++)
+	{
+		this->objects[i]->Render(context);
+	}
 }
+
--- Prodigium/SceneHandler.cpp
+++ Prodigium/SceneHandler.cpp
@@ -1,27 +1,50 @@
 #include "SceneHandler.h"
 
-void SceneHandler::ForestScene()
-{
-	/*
-		Create all the scene objects in here with the help of Scene class.
-	*/
-	Scene forestLevel;
-
-	// Example way to add a tree.
-	//forestLevel.Add("/objects/tree.obj", 10.0f, 1.0f, 10.0f, 0.0f);
-}
-
 SceneHandler::SceneHandler()
 {
-
+	this->AddScene();
 }
 
 SceneHandler::~SceneHandler()
 {
-	// Empty
 }
 
-void SceneHandler::Render(ID3D11DeviceContext*& context, int sceneIndex)
+void SceneHandler::AddScene()
 {
-	scenes[sceneIndex]->Render(context);
+	Scene newScene;
+	scenes.push_back(newScene);
+	this->currentScene = (int)scenes.size() - 1;
+}
+
+void SceneHandler::RemoveScene(int index)
+{
+	this->currentScene = 0;
+	this->scenes.erase(this->scenes.begin() + index);
+}
+
+Scene& SceneHandler::EditScene()
+{
+	return this->scenes[this->currentScene];
+}
+
+void SceneHandler::SwitchScene(int index)
+{
+	if (index < (int)scenes.size() && index >= 0)
+	{
+		this->currentScene = index;
+	}
+	else
+	{
+		printf("You went too far %d is above %d\n", index, (int)scenes.size() - 1);
+	}
+}
+
+int SceneHandler::GetNrOfScenes() const
+{
+	return (int)scenes.size();
+}
+
+void SceneHandler::Render(ID3D11DeviceContext*& context)
+{
+	this->scenes[this->currentScene].Render(context);
 }
