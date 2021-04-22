41824049e29bc59ae76c2895d566c8c369736ab0
Swizzman <swizzman123@gmail.com>
2021-04-22 10:55:44 +0200

    Keyboard now works

--- Prodigium/Engine.cpp
+++ Prodigium/Engine.cpp
@@ -61,7 +61,10 @@ bool Engine::StartUp(HINSTANCE& instance, const UINT& width, const UINT& height)
 	{
 		return false;
 	}
-
+	if (!InputHandler::Initialize(window.GetWindowHandler()))
+	{
+		return false;
+	}
 	ResourceManager::Initialize();
 
 	Graphics::SetMainWindowViewport();
--- Prodigium/Engine.h
+++ Prodigium/Engine.h
@@ -5,10 +5,9 @@
 #include <io.h>
 #include "ResourceManager.h"
 #include "RenderPass.h"
+#include "InputHandler.h"
+#include "MeshObject.h"
 #include "SceneHandler.h"
-
-#include "MeshObject.h"		//DELETE LATER***
-#include "CameraObject.h"	//DELETE LATER***
 
 class Engine
 {
