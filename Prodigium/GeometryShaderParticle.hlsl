9e82b25e86cd7b425c061c3d0cc53f83dfb502fb
DAVID WINGQVIST <daag19@student.bth.se>
2021-05-05 20:05:57 +0200

    optimizing

--- Prodigium/GeometryShaderParticle.hlsl
+++ Prodigium/GeometryShaderParticle.hlsl
@@ -37,15 +37,20 @@ void main(point VertexOut input[1], uint primID : SV_PrimitiveID,
 {
     float3 position = input[0].pos;
     float3 toCamera = camPos.xyz - position.xyz;
+    
+    // Exclude particles that are too far away from the player.
+    if (length(toCamera) > 500.0f)
+        return;
+    
     toCamera.y = 0.0f;
     toCamera = normalize(toCamera);
     float3 up = float3(0.0f, 1.0f, 0.0f);
     float3 right = cross(up, toCamera);
 
-    float width = 0.3f;
+    float width = 0.25f;
     float height = 0.6f;
     
-    float4 pos_w[4] =
+    float4 positionW[4] =
     {
         float4(position + width * right - height * up, 1.0f),
 		float4(position + width * right + height * up, 1.0f),
@@ -57,7 +62,7 @@ void main(point VertexOut input[1], uint primID : SV_PrimitiveID,
 	[unroll]
     for (int i = 0; i < 4; i++)
     {
-        geOut.pos = mul(mul(pos_w[i], view), proj);
+        geOut.pos = mul(mul(positionW[i], view), proj);
         output.Append(geOut);
     }
 }
