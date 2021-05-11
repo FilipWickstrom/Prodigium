2acffdf516f827b189baadb88d080d787f233ac3
Swizzman <swizzman123@gmail.com>
2021-05-11 10:51:40 +0200

    small addition to skybox

--- Prodigium/SkyboxPixelShader.hlsl
+++ Prodigium/SkyboxPixelShader.hlsl
@@ -12,6 +12,7 @@ struct InputFromVertex
 
 float4 main(InputFromVertex input) : SV_TARGET
 {
+    //TODO: Fix the skybox so it is the proper colour
     return float4(0.6f, 0.6f, 0.6f, 0.0f);
     //return cubemap.Sample(samplerState, input.texCoord);
 }
