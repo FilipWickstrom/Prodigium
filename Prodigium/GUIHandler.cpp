2fc298d84b2233300c2913b9c97c6b3ac0e74f1b
MARTIN HÖGLUND <mahh18@student.bth.se>
2021-05-21 14:18:49 +0200

    Big GUI style overhaul

--- Prodigium/GUIHandler.cpp
+++ Prodigium/GUIHandler.cpp
@@ -85,9 +85,9 @@ void SetUpGUIStyleGame()
     style.Colors[ImGuiCol_PopupBg]                                  = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_Border]                                   = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_BorderShadow]                             = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_FrameBg]                                  = ImVec4(46.f/255.f, 68.f/255.f, 138.f/255.f, 1.f);
-    style.Colors[ImGuiCol_FrameBgHovered]                           = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_FrameBgActive]                            = ImVec4(0.f, 0.f, 0.f, 0.f);
+    style.Colors[ImGuiCol_FrameBg]                                  = ImVec4(0.f, 0.f, 0.f, 0.6f);
+    style.Colors[ImGuiCol_FrameBgHovered]                           = ImVec4(1.f, 1.f, 1.f, 0.5f);
+    style.Colors[ImGuiCol_FrameBgActive]                            = ImVec4(0.2f, 0.2f, 0.2f, 0.7f);
     style.Colors[ImGuiCol_TitleBg]                                  = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_TitleBgCollapsed]                         = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_TitleBgActive]                            = ImVec4(0.f, 0.f, 0.f, 0.f);
@@ -96,9 +96,9 @@ void SetUpGUIStyleGame()
     style.Colors[ImGuiCol_ScrollbarGrab]                            = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_ScrollbarGrabHovered]                     = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_ScrollbarGrabActive]                      = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_CheckMark]                                = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_SliderGrab]                               = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_SliderGrabActive]                         = ImVec4(0.f, 0.f, 0.f, 0.f);
+    style.Colors[ImGuiCol_CheckMark]                                = ImVec4(1.f, 1.f, 1.f, 1.f);
+    style.Colors[ImGuiCol_SliderGrab]                               = ImVec4(1.f, 1.f, 1.f, 1.f);
+    style.Colors[ImGuiCol_SliderGrabActive]                         = ImVec4(0.f, 0.f, 0.f, 0.6f);
     style.Colors[ImGuiCol_Button]                                   = ImVec4(0.f, 0.f, 0.f, 0.6f);
     style.Colors[ImGuiCol_ButtonHovered]                            = ImVec4(1.f, 1.f, 1.f, 0.5f);
     style.Colors[ImGuiCol_ButtonActive]                             = ImVec4(1.f, 1.f, 1.f, 1.f);
@@ -113,49 +113,6 @@ void SetUpGUIStyleGame()
     style.Colors[ImGuiCol_PlotHistogram]                            = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_PlotHistogramHovered]                     = ImVec4(0.f, 0.f, 0.f, 0.f);
     style.Colors[ImGuiCol_TextSelectedBg]                           = ImVec4(0.f, 0.f, 0.f, 0.f);
-}
-
-void SetUpGUIStyleMainMenu()
-{
-    ImGuiStyle& style = GetStyle();
-
-
-    style.Alpha = 1.0f;
-    style.FrameRounding = 0;
-    style.Colors[ImGuiCol_Text] = ImVec4(0.6f, 0.6f, 1.0f, 1.f);
-    style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.f, 0.f, 0.f, 1.f);
-    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_PopupBg] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_Border] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_FrameBg] = ImVec4(46.f / 255.f, 68.f / 255.f, 138.f / 255.f, 1.f);
-    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(46.f / 255.f, 68.f / 255.f, 138.f / 255.f, 0.6f);
-    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(46.f / 255.f, 68.f / 255.f, 138.f / 255.f, 0.6f);
-    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.6f, 0.6f, 1.0f, 1.f);
-    style.Colors[ImGuiCol_SliderGrab] = ImVec4(1.f, 1.f, 1.f, 0.6f);
-    style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.6f, 0.6f, 1.0f, 0.6f);
-    style.Colors[ImGuiCol_Button] = ImVec4(0.f, 0.f, 0.f, 0.6f);
-    style.Colors[ImGuiCol_ButtonHovered] = ImVec4(1.f, 1.f, 1.f, 0.5f);
-    style.Colors[ImGuiCol_ButtonActive] = ImVec4(1.f, 1.f, 1.f, 1.f);
-    style.Colors[ImGuiCol_Header] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_PlotLines] = ImVec4(0.f, 0.f, 0.f, 1.f);
-    style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.f, 0.f, 0.f, 0.f);
-    style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.f, 0.f, 0.f, 0.f);
 }
 
 const bool GUIHandler::Initialize(const HWND& window)
@@ -212,7 +169,7 @@ void GUIHandler::Render(int playerHp, int clues, float& timer1, float& timer2, O
 
     if (GUIHandler::instance->showMainMenu)
     {
-        SetUpGUIStyleMainMenu();
+        SetUpGUIStyleGame();
         GUIHandler::instance->RenderMainMenu();
     }
     if (GUIHandler::instance->showGameGUI)
@@ -225,7 +182,6 @@ void GUIHandler::Render(int playerHp, int clues, float& timer1, float& timer2, O
     }
     if (GUIHandler::instance->showOptionsMenu)
     {
-        SetUpGUIStyleMainMenu();
         GetIO().WantCaptureMouse = true;
         GetIO().WantCaptureKeyboard = true;
         GetIO().MouseDrawCursor = true;
@@ -573,7 +529,7 @@ void GUIHandler::RenderPauseMenu()
 void GUIHandler::RenderMainMenu()
 {
     bool* isActive = new bool;
-    SetNextWindowPos(ImVec2((float)(Graphics::GetWindowWidth() / 2) - 125, (float)Graphics::GetWindowHeight() - 150));
+    SetNextWindowPos(ImVec2((float)(Graphics::GetWindowWidth() / 2) - 150, (float)Graphics::GetWindowHeight() - 150));
     SetNextWindowSize(ImVec2(500, 600), 0);
     
     Begin("MENU", isActive, ImGuiWindowFlags_NoTitleBar);
